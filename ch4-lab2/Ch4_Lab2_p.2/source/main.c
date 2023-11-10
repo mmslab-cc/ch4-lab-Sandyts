ff114e3cba7320a4611fd0c1fd6a1d405836f88f
Sandyts <st1005127@gmail.com>
2023-11-10 16:43:25 +0800

    m

--- ch4-lab2/Ch4_Lab2_p.2/source/main.c
+++ ch4-lab2/Ch4_Lab2_p.2/source/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int abbyone(int);
+
 int main(void)
 {
+	int x = 100;
+	int y = abbyone(x);
+	printf("x=%d\n", x);
 	system("pause");
 	return 0;
+}
+
+int abbyone(int x) {
+	x++;
+	printf("x=%d\n", x);
+	return x;
 }
